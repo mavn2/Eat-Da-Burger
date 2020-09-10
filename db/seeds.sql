702ded78d6046f56d5661dd618ed3b6be7dfd7f1
Max Nicolai <maxvnicolai@gmail.com>
2020-09-10 16:45:48 -0700

    created schema + seeds files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers 
+  (burger_name, devoured)
+VALUES
+  ('baconnator', true),
+  ('mcrib', true),
+  ('lunchbox lab(oratory)', true),
+  ('the impossible burger', false),
+  ("Dick's", true),
+  ('a bad burger', false);
