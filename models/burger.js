ce03c6981742e98a800a7b934955f4d93b630b86
Max Nicolai <maxvnicolai@gmail.com>
2020-09-12 18:04:37 -0700

    Added first draft controller methods for all three sql queries

--- models/burger.js
+++ models/burger.js
@@ -4,13 +4,29 @@ const orm = require('../config/orm.js');
 //Burger object w/ methods used to call ORM methods
 //cb stands for callback, function called/passed in controller
 const burger = {
+  //Gets burger data from table and executes function passed in controller
   getAll: (cb) => {
-    orm.selectAll(
-      (data) => {
+    orm.selectAll(data => {
         cb(data);
       }
     );
   },
 
+  //Adds burger w/ given value and '  '
+  add: (value, cb) => {
+    orm.insertOne(value, data => {
+        cb(data)
+      }
+    );
+  },
 
+  //Updates burger w/ given id and ' '
+  devour: (id, cb) => {
+    orm.updateOne(id, data => {
+        cb(data)
+      }
+    );
+  }
 }
+
+module.exports(burger)
