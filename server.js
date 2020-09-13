cfa2abe70ae28e8b68f3e8328a5e05e82853ed4a
Max Nicolai <maxvnicolai@gmail.com>
2020-09-12 19:55:34 -0700

    created server file, server runs and seed data displays as intended

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
+//Dependencies
 const express = require('express');
+const exphbs = require('express-handlebars')
+const routes = require('./controllers/burgers_controller.js')
+
+//Create an express server
+const app = express();
+const PORT = process.env.PORT ||  8080;
+
+//Additional express parsing utilities
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set handlebars as templating engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+//Sets app to use specified routes
+app.use(routes);
+
+//Start server
+app.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
+});
