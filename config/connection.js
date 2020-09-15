b38c7e1b090fc1d8dde705bca1516bba65cc48e8
Max Nicolai <maxvnicolai@gmail.com>
2020-09-14 17:26:04 -0700

    added JawsDB connection parameters

--- config/connection.js
+++ config/connection.js
@@ -5,14 +5,17 @@ const mysql = require('mysql');
 //Required files
 const password = require('./password');
 
-//Create connection object w/ mysql
-const connection = mysql.createConnection({
+//Sets connection parameters for hosted or local setting
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env,JAWSDB_URL)
+} else {
+  const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: password,
   database: 'burgers_db'
-});
+})};
 
 connection.connect( (err) => {
   if (err) {
