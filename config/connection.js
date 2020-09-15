11c61f5d3e8e5e9b006f3f0dc30e6095fb130eaf
Max Nicolai <maxvnicolai@gmail.com>
2020-09-14 18:14:53 -0700

    fixed issues with connection declarations scope, app runs locally

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,22 @@
 //Dependencies
 const mysql = require('mysql');
 require('dotenv').config()
+let connection;
 
 //Sets connection parameters for hosted or local setting
 if (process.env.JAWSDB_URL) {
   //Creates connection based on environmental variable
-  connection = mysql.createConnection(process.env,JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
 } else {
   //Creates connection
-  const connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
   password: process.env.password,
   database: 'burgers_db'
-})};
+  });
+};
 
 connection.connect( (err) => {
   if (err) {
